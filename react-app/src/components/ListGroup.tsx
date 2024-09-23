fc5d04bf11789d105c95cb24238d42bf5fc87a76
johntrehearn <johntrehearnbc@gmail.com>
2024-09-23 14:53:25 +0300

    notes

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -16,7 +16,9 @@ function ListGroup() {
     const getMessage = () => {
         return items.length === 0 ? <p>No items to display</p> : null;
     };
-    
+
+    // It is better to use a function as functions can have paramenters. So you can do different things based on the parameters / different conditions.
+
     return (
         <>
         <h1>Locations</h1>
