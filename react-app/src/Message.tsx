d24f74f0daf6e0d4b0c5397ae6315414a9b2d91d
johntrehearn <johntrehearnbc@gmail.com>
2024-09-23 14:02:34 +0300

    dynamically add JS statement

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,6 +1,11 @@
+// in the braces{} we can write any javascript expression that returns a value
+// we can also write JS statements
 function Message() {
     const name = 'John';
-    return <h1>Hello {name}</h1>;
+    if (name) {
+        return <h1>Hello {name}</h1>;
+    }
+    return <h1>Hello Guest</h1>;
 }
 
 export default Message;
