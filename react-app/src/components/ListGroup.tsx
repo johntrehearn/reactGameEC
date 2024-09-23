30e2c2d1ee8c9db1da694c1b06792e9e2b9d71d0
johntrehearn <johntrehearnbc@gmail.com>
2024-09-23 14:49:46 +0300

    message as a constant

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,7 +1,6 @@
 // In react components you can render other react components or html elements.
 
-//  THE ONLY EXCEPTION IS IF YOU USE BRACES {} then you can render any javascript expression.
-
+//  THE ONLY EXCEPTION IS IF YOU USE BRACES {} then
 function ListGroup() {
 
     let items = [
@@ -13,11 +12,13 @@ function ListGroup() {
     ];
 
     items = [];
+
+    const message = items.length === 0 ? <p>No items to display</p> : null;
     
     return (
         <>
         <h1>Locations</h1>
-        {items.length === 0 ? <p>No items to display</p> : null}
+        {message}
         <ul className="list-group">
             {items.map(item => <li key={item}>{item}</li>)}
         </ul>
